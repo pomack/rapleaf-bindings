069c936b30edfee168522c80728f675a0c21257f
Aalok Shah <aalok@shah.ws>
2010-08-01 22:27:21 -0400

    Add/Update License Headers to source files

--- python/rapleaf.py
+++ python/rapleaf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-# $Header:   $
-# Copyright 2010 Aalok Shah (aalok@shah.ws)
+# Copyright 2010 Aalok Shah
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
